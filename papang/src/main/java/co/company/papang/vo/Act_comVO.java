897f340d2cda2c0fc14eb5d3cfb82ae855de84b8
Kim Sang Min <yhoo112@naver.com>
2020-12-15 17:47:13 +0900

    VO객체만들기

---
+++ papang/src/main/java/co/company/papang/vo/Act_comVO.java
@@ -0,0 +1,11 @@
+package co.company.papang.vo;
+
+public class Act_comVO {
+	private String play_no;
+	private String play_title;
+	private String play_content;
+	private String play_pic;
+	private String mbr_id;
+	private String play_hit;
+	private String play_category;
+}
---
+++ papang/src/main/java/co/company/papang/vo/Sitter_revVO.java
@@ -0,0 +1,9 @@
+package co.company.papang.vo;
+
+public class Sitter_revVO {
+	private String sit_mbr_id;
+	private String mbr_id;
+	private String sc_no;
+	private String sc_content;
+	private String sc_rate;
+}
