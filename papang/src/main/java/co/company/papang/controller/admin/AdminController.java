dd32d5d017e2c39571ac9d23111a99691a9362c5
Kim Sang Min <yhoo112@naver.com>
2020-12-15 16:04:23 +0900

    컨트롤러, 뷰페이지 추가

---
+++ papang/src/main/java/co/company/papang/controller/admin/AdminController.java
@@ -0,0 +1,18 @@
+package co.company.papang.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping("/admin") //url 예전 .do
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("admin/test"); //jsp주소
+	}
+}
