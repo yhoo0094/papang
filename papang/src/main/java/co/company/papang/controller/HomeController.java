cf77fcb4935695b87560426a52fe8154b3e526ee
Kim Sang Min <yhoo112@naver.com>
2020-12-16 12:57:02 +0900

    폼xml jstl추가

--- papang/src/main/java/co/company/papang/controller/HomeController.java
+++ papang/src/main/java/co/company/papang/controller/HomeController.java
@@ -15,4 +15,9 @@ public class HomeController {
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("index");
 	}
+	
+	@RequestMapping(value="/normalBoard")
+	public String noticeBoard() {
+		return "normal/normalBoard";
+	}
 }
