c02c77b70f6219a0039f0ebb02c6178be51e96ea
Kim Sang Min <yhoo112@naver.com>
2021-01-12 10:06:24 +0900

    엑셀 업로드 폼이랑 콘피그

--- papang/src/main/java/co/company/papang/config/MvcConfiguration.java
+++ papang/src/main/java/co/company/papang/config/MvcConfiguration.java
@@ -10,38 +10,46 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan(basePackages="co.company.papang")
+@ComponentScan(basePackages = "co.company.papang")
 @EnableWebMvc
-public class MvcConfiguration implements WebMvcConfigurer{
+public class MvcConfiguration implements WebMvcConfigurer {
 
-	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
 	}
-	
+
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-	   	configurer.enable();
-		}	
+		configurer.enable();
+	}
 
 	@Bean
-	public ViewResolver getViewResolver(){
+	public ViewResolver getViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		resolver.setOrder(3);
 		return resolver;
 	}
-	 @Bean CommonsMultipartResolver multipartResolver() {
-		   CommonsMultipartResolver multi =
-				   new CommonsMultipartResolver();
-		   multi.setMaxUploadSize(1024*10000);
-		   return multi;
-	   }
+
+	@Bean
+	CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver multi = new CommonsMultipartResolver();
+		multi.setMaxUploadSize(1024 * 10000);
+		return multi;
+	}
 	
+	@Bean
+	BeanNameViewResolver beanNameViewResolver() {
+		BeanNameViewResolver bean = new BeanNameViewResolver();
+		bean.setOrder(1);
+		return bean;
+	}
+
 }
